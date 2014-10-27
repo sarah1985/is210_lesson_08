19916d04873a5f2ad290d25ecef2e4f62bf3f6fd
sarah1985 <sarahmleavy@gmail.com>
2014-10-27 12:44:02 -0400

    4

--- task_04.py
+++ task_04.py
@@ -1,1 +1,15 @@
-__author__ = 'Sarah'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 04: Subclassing"""
+
+import task_02
+
+class Tigerpaw(task_02.Tire):
+    """override maximum miles"""
+
+    def __init__(self, miles=0):
+        self.miles = miles
+        self.__maximum_miles = 750
+
+CAR_TEST = task_02.Car()
+print CAR_TEST.tires
